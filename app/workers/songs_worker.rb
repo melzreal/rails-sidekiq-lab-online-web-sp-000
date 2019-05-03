fe8c49c4882475022f8f926554e172a5905abb5b
Melissa Real <melissa.real@hotmail.com>
2019-05-03 16:01:05 +0000

    Automatically backed up by Learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -4,7 +4,7 @@ class SongsWorker
 
   def perform(songs_file)
     CSV.foreach(songs_file, headers: true) do |song|
-      Song.create(title: song[0], first_name: song[1], last_name: song[2])
+      Song.create(title: song[0], artist_name: song[1])
     end
   end
 end
