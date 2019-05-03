c816dccc5c5e78746188a204f2804c2fb0ad05cf
Melissa Real <melissa.real@hotmail.com>
2019-05-03 15:59:40 +0000

    Automatically backed up by Learn

---
+++ app/workers/songs_worker.rb
@@ -0,0 +1,10 @@
+class SongsWorker
+  require 'csv'
+  include Sidekiq::Worker
+
+  def perform(songs_file)
+    CSV.foreach(songs_file, headers: true) do |song|
+      Song.create(title: song[0], first_name: song[1], last_name: song[2])
+    end
+  end
+end
